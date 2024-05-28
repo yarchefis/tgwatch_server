c3afa7a3b327339f390179b78612269d227d63f6
yarchefis <yaroslavfis4@gmail.com>
2024-05-28 20:59:24 +0300

    full

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo -e "\e[47m\e[30m"
+echo "████████╗ ██████╗ ██╗    ██╗ █████╗ ████████╗ ██████╗██╗  ██╗"
+echo "╚══██╔══╝██╔      ██║    ██║██╔══██╗╚══██╔══╝██╔════╝██║  ██║"
+echo "   ██║   ██║ ██║  ██║ █╗ ██║███████║   ██║   ██║     ███████║"
+echo "   ██║   ██║   ██║██║███╗██║██╔══██║   ██║   ██║     ██╔═ ██║"
+echo "   ██║   ╚██████╔╝╚███╔███╔╝██║  ██║   ██║   ╚██████╗██║  ██║"
+echo "   ╚═╝    ╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═╝   ╚═╝    ╚═════╝╚═╝  ╚═╝"
+echo -e "\e[0m"
+
+echo "Downloading dependencies..."
+sudo apt update -qq > /dev/null 2>&1
+sudo apt install -y git python3 python3-pip -qq > /dev/null 2>&1
+
+echo "Cloning repository..."
+git clone https://github.com/yarchefis/tgwatch_server > /dev/null 2>&1
+
+echo "Installing libraries..."
+pip3 install "flask[async]" telethon > /dev/null 2>&1
+
+echo "Running the application..."
+cd tgwatch_server
+python3 app.py
